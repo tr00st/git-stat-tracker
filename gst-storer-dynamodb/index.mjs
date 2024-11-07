5e560661e746023e33562deca1d0d33765329444
James Cheese <trust@tr00st.co.uk>
2024-11-07 18:00:54 +0100

    Beginning args support for dynamodb

--- gst-storer-dynamodb/index.mjs
+++ gst-storer-dynamodb/index.mjs
@@ -1,5 +1,28 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+const argv = await yargs(hideBin(process.argv))
+  .usage('$0 <inputFile>', 'parses the results from inputFile', (yargs) => {
+    yargs.positional('inputFile', {
+      description: 'Specify the file to be read - must be an EsLint produced JSON report file',
+      type: 'string'
+    })
+    .option('outputFormat', {
+      alias: 'f',
+      type: 'string',
+      description: 'Specify the output format to be used when writing to file',
+      choices: ['json', 'csv']
+    })
+    .option('outputFile', {
+      alias: 'o',
+      type: 'string',
+      description: 'Specify the filename where you want the output to be written'
+    })
+  })
+  .demand(1)
+  .parseAsync();
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
