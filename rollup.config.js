92f20013b9ab9664b87353c5c576fc2864222823
James Cheese <trust@tr00st.co.uk>
2024-11-15 17:03:51 +0000

    Switching to rollup for builds becaue I can't be bothered any more

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'es',
+    preserveModules: true, // This keeps your file structure
+    entryFileNames: '[name].js'
+  },
+  plugins: [
+    commonjs({
+      // This helps with default imports from CommonJS modules
+      defaultIsModuleExports: true
+    }),
+    typescript({
+      tsconfig: './tsconfig.json'
+    }),
+    nodeResolve(),
+    json()
+  ],
+  external: [
+    /node_modules/
+  ]
+}
